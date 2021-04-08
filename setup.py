bc4ba90a980f3d723d2f84d67369cb4557446294
lemontheme <lemontheme@gmail.com>
2021-04-08 22:43:08 +0200

    Move from single module -> pkg to better control symbol imports

--- setup.py
+++ setup.py
@@ -1,45 +1,38 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from pathlib import Path
 
 
-ROOT = Path(__file__).parent
-SRC_MODULE = ROOT / "src/sopredictable.py"
-
+SRC_ROOT = Path(__file__).parent / "src"
+ABOUT_MODULE = SRC_ROOT / "sopredictable/about.py"
 
 install_requires = [
     "typing-extensions;python_version < '3.8'",
 ]
 
 extras_require = {"dev": ["pytest"]}
+    
+with ABOUT_MODULE.open("r") as f:
+    mod_globals = {}
+    exec(f.read(), mod_globals)
+
+version = mod_globals["__version__"]
+del mod_globals
 
 
-def setup_pkg():
-
-    with SRC_MODULE.open("r") as f:
-        mod_globals = {}
-        exec(f.read(), mod_globals)
-
-    version = mod_globals["__version__"]
-    del mod_globals
-
-    setup(
-        name="sopredictable",
-        version=version,
-        author="Adriaan Lemmens (lemontheme)",
-        author_email="lemontheme@gmail.com",
-        url="https://github.com/lemontheme/sopredictable",
-        package_dir={"": "src"},
-        py_modules=["sopredictable"],
-        install_requires=install_requires,
-        extras_require=extras_require,
-        python_requires=">=3.7",
-        classifiers=[
-            "Programming Language :: Python :: 3",
-            "License :: OSI Approved :: MIT License",
-            "Operating System :: OS Independent",
-        ],
-    )
-
-
-if __name__ == "__main__":
-    setup_pkg()
+setup(
+    name="sopredictable",
+    version=version,
+    author="Adriaan Lemmens (lemontheme)",
+    author_email="lemontheme@gmail.com",
+    url="https://github.com/lemontheme/sopredictable",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires=install_requires,
+    extras_require=extras_require,
+    python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
